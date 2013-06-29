8e76595bef110bb9b00e5dc3c53f7d2fda970d85
Taylor Russ <stahler84@gmail.com>
2013-06-29 13:17:30 -0700

    Adding xmonad config, xinitrc, IDLE config

--- debian-6.0.7/manifests/default.pp
+++ debian-6.0.7/manifests/default.pp
@@ -1,6 +1,17 @@
 # Default (read: test) puppet manifest for debian dev box
+# Mirroring a test setup (done manually) for now.
+# TODO:
+#  - better way to create parent directories?
+#  - variablize username and configure box to add user
+#
+
 exec { "apt-get update":
 	path => "/usr/bin",
+}
+
+# ugly but works - TODO fix this later
+exec{'mkdir -p /root/.idlerc':
+				path => "/bin",
 }
 
 # install xinit for basic window system
@@ -27,3 +38,38 @@ package { "sakura":
 	require => Exec["apt-get update"],
 }
 
+# Development - install IDLE
+package { "idle":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+# deploy .xinitrc to start xmonad
+file { '.xinitrc':
+  name          => '/root/.xinitrc',
+  ensure        => present,
+  source        => '/vagrant/files/xinitrc',
+  owner         => root,
+  group         => root,
+  mode          => 0640,
+}
+
+# deploy IDLE skins to prevent eye bleeds
+file { 'config-highlight.cfg':
+  name          => '/root/.idlerc/config-highlight.cfg',
+  ensure        => present,
+  source        => '/vagrant/files/config-highlight.cfg',
+  owner         => root,
+  group         => root,
+  mode          => 0640,
+}
+
+# point IDLE to correct skin
+file { 'config-main.cfg':
+  name          => '/root/.idlerc/config-main.cfg',
+  ensure        => present,
+  source        => '/vagrant/files/config-main.cfg',
+  owner         => root,
+  group         => root,
+  mode          => 0640,
+}
