e6b6a018a0630e11cd4fe2561a117768f21ba315
Taylor Russ <stahler84@gmail.com>
2013-10-24 19:11:00 -0700

    adding puppet folder as well.

---
+++ puppet/manifests/debian-7.1.0.pp
@@ -0,0 +1,143 @@
+# Default (read: test) puppet manifest for debian dev box
+# Mirroring a test setup (done manually) for now.
+# TODO:
+#  - variablize username and configure box to add user
+#
+
+######################################################
+# Folder Prep Work
+######################################################
+
+# Ensure idlerc folder exists
+file { "/root/.idlerc":
+  ensure => 'directory',
+}
+
+######################################################
+# EXEC COMMANDS
+######################################################
+
+exec { "apt-get update":
+	path => "/usr/bin",
+}
+
+
+######################################################
+# PACKAGES
+######################################################
+
+
+# install xinit for basic window system
+package { "xinit":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+# install  x11-xserver-utils 
+package { "x11-xserver-utils":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+# virtualbox: install x11 guest addition drivers
+package { "virtualbox-ose-guest-x11":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+# install  build-essential
+package { "build-essential":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+# install xmonad for tiling
+package { "xmonad":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+# fluxbox for fun
+package { "fluxbox":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+# ..and sakura for easy pretty colors
+package { "sakura":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+# chromium-browser
+package { "chromium-browser":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+
+# Development - install IDLE
+package { "idle":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+# Development - install eclipse
+package { "eclipse":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+# Development - install VIM
+package { "vim":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+
+######################################################
+# CONFIGURATION FILES
+######################################################
+
+
+# deploy .xinitrc to start xmonad
+file { '.xinitrc':
+  name          => '/root/.xinitrc',
+  ensure        => present,
+  source        => '/vagrant/files/xinitrc',
+  owner         => root,
+  group         => root,
+  mode          => 0640,
+}
+
+file { ".config":
+  name					=> '/root/.config',
+  ensure 				=> directory,
+	source 				=> '/vagrant/files/config',
+  recurse 			=> true,
+  owner 				=> "root",
+  group 				=> "root",
+  mode 					=> 0644,
+}
+
+# deploy IDLE skins to prevent eye bleeds
+file { 'config-highlight.cfg':
+  name          => '/root/.idlerc/config-highlight.cfg',
+  ensure        => present,
+  source        => '/vagrant/files/config-highlight.cfg',
+  owner         => root,
+  group         => root,
+  mode          => 0640,
+	require				=> File['/root/.idlerc'],
+}
+
+# point IDLE to correct skin
+file { 'config-main.cfg':
+  name          => '/root/.idlerc/config-main.cfg',
+  ensure        => present,
+  source        => '/vagrant/files/config-main.cfg',
+  owner         => root,
+  group         => root,
+  mode          => 0640,
+  require				=> File['/root/.idlerc'],
+}
